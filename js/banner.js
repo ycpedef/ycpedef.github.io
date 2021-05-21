193b390cc5b2299f5b8a3a0361301644cf851f84
ycpedef <ycpedef@foxmail.com>
2021-05-22 00:08:53 +0800

    Site updated: 2021-05-22 00:08:41

---
+++ js/banner.js
@@ -0,0 +1,19 @@
+var page = document.getElementsByClassName("full_page");
+if (page.length != 0) {
+  page[0].style.background = "transparent";
+}
+page = document.getElementsByClassName("post-bg");
+if (page.length != 0) {
+  page[0].style.background = "transparent";
+}
+//var sty=document.createElement('style');
+//sty.innerText='post-bg:before{background-color:\'\'}';
+//document.body.appendChild(sty);
+page = document.getElementsByClassName("not-index-bg");
+if (page.length != 0) {
+  page[0].style.background = "transparent";
+}
+page = document.getElementsByClassName("not-index-bg");
+if (page.length != 0) {
+  page[0].style.background = "transparent";
+}
